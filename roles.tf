6bd9634b7f381e8b93b50349aa77caa64aeeaf0e
Yves Vogl <yves.vogl@adesso.de>
2022-07-18 11:17:12 +0200

    Nailing the components which lead to cycle error

--- roles.tf
+++ roles.tf
@@ -166,6 +166,7 @@ locals {
     wvd_applications                           = local.combined_objects_wvd_applications
     wvd_host_pools                             = local.combined_objects_wvd_host_pools
     wvd_workspaces                             = local.combined_objects_wvd_workspaces
+    virtual_machine_scale_sets                 = local.combined_objects_virtual_machine_scale_sets
     log_analytics                              = local.current_objects_log_analytics
     route_tables                               = local.combined_objects_route_tables
   }
